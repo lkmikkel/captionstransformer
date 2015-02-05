c0272da79dce731fb859f576100e93391d4e039d
lkmikkel <lkm@waoo.dk>
2015-02-05 08:42:23 +0100

    Who uses a HTML parser for XML??? ARGHHgit add captionstransformer/ttml.py! This fucks up XML namespaces. Added support for multiline captions in reader

--- captionstransformer/ttml.py
+++ captionstransformer/ttml.py
@@ -4,19 +4,26 @@ from captionstransformer import core
 
 class Reader(core.Reader):
     def text_to_captions(self):
-        soup = BeautifulSoup(self.rawcontent)
+        soup = BeautifulSoup(self.rawcontent, 'xml')
         texts = soup.find_all('p')
         for text in texts:
             caption = core.Caption()
             caption.start = self.get_date(text['begin'])
             caption.end = self.get_date(text['end'])
-            caption.text = text.text
+            cap = u''
+            for item in text.contents:
+                line = u'%s' % item
+                if line != '<br/>':
+                    cap += line
+                else:
+                    cap += '\n'
+            caption.text = cap
             self.add_caption(caption)
 
         return self.captions
 
     def get_date(self, time_str):
-        return datetime.strptime(time_str, '%H:%M:%S')
+        return datetime.strptime(time_str, '%H:%M:%S.%f')
 
 class Writer(core.Writer):
     DOCUMENT_TPL = u"""<tt xml:lang="" xmlns="http://www.w3.org/ns/ttml"><body><div>%s</div></body></tt>"""
@@ -26,5 +33,5 @@ class Writer(core.Writer):
         """Return start and end time for the given format"""
         #should be seconds by default
 
-        return {'start': caption.start.strftime('%H:%M:%S'),
-                'end': caption.end.strftime('%H:%M:%S')}
+        return {'start': caption.start.strftime('%H:%M:%S.%f')[:-3],
+                'end': caption.end.strftime('%H:%M:%S.%f')[:-3]}
